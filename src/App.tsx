44fd14baf67fbf60e56200e57cac4ed80c89da05
byung ju choi <bjc30849@gmail.com>
2022-01-12 17:47:24 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+import {GlobalStyle} from "./styles/global-styles"
+import {motion } from "framer-motion"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle/>
+      <Wrapper>
+        <Box
+          transition={{delay:0.5, type:"spring", }} 
+          initial={{scale:0}}  
+          animate={{scale:1, rotateZ:360}}/>
+      </Wrapper>
+    </>
   );
 }
+
+const Wrapper  = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Box = styled(motion.div)` // Framer-motion과 styled컴포넌트를 사용하는 방법
+  width: 200px;
+  height: 200px;
+  background-color: red;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0,0,0,0.1), 0 10px 20px rgba(0,0,0,0.6);
+`
 
 export default App;
